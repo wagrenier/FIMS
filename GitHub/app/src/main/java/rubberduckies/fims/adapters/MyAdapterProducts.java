f29fa2e9c8090709201f8457608179fdf739cefa
EtienneBerube <tiennebrb@gmail.com>
2018-01-27 15:16:57 -0500

    main Activity started
    adapters for products is about to be imported

---
+++ GitHub/app/src/main/java/rubberduckies/fims/adapters/MyAdapterProducts.java
@@ -0,0 +1,10 @@
+package rubberduckies.fims.adapters;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Etienne on 27/01/2018.
+ */
+
+public class MyAdapterProducts extends RecyclerView.Adapter<Products> {
+}
---
+++ GitHub/app/src/main/java/rubberduckies/fims/adapters/MyViewHolderProducts.java
@@ -0,0 +1,26 @@
+package rubberduckies.fims.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Etienne on 27/01/2018.
+ */
+
+
+
+public class MyViewHolderProducts extends RecyclerView.ViewHolder {
+    private ImageView button;
+    private TextView name;
+    private TextView upc;
+
+    public MyViewHolderProducts(View itemView) {
+        super(itemView);
+        button = (Button) itemView.findViewById(R.id.button);
+        name = (TextView) itemView.findViewById(R.id.name);
+        upc = (TextView) itemView.findViewById(R.id.upc);
+    }
+}
